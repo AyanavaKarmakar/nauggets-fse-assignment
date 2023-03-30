0058460b3e5a0b0f4a25fc29e552293d8f467d28
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-30 20:33:06 +0530

    feat: create gold transcation model

---
+++ src/models/goldTransaction.ts
@@ -0,0 +1,97 @@
+import mongoose, { Schema } from "mongoose";
+
+interface IGoldTransaction {
+  userId: mongoose.Types.ObjectId;
+  entityUser: mongoose.Types.ObjectId;
+  quantity: number;
+  amount: number;
+  type: "CREDIT" | "DEBIT";
+  status: "FAILED" | "SUCCESS" | "WAITING" | "CANCELED" | "PENDING";
+  runningBalance: {
+    wallet: number;
+    gold: number;
+    goldPrice: number;
+  };
+}
+
+interface GoldTransactionModel extends mongoose.Model<IGoldTransaction> {
+  build(todo: IGoldTransaction): IGoldTransaction;
+}
+
+interface GoldTransactionDoc extends mongoose.Document {
+  userId: mongoose.Types.ObjectId;
+  entityUser: mongoose.Types.ObjectId;
+  quantity: number;
+  amount: number;
+  type: "CREDIT" | "DEBIT";
+  status: "FAILED" | "SUCCESS" | "WAITING" | "CANCELED" | "PENDING";
+  runningBalance: {
+    wallet: number;
+    gold: number;
+    goldPrice: number;
+  };
+}
+
+const goldTransactionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+
+  entityUser: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+
+  quantity: {
+    type: Number,
+    required: true,
+  },
+
+  amount: {
+    type: Number,
+    required: true,
+  },
+
+  type: {
+    type: String,
+    enum: ["CREDIT", "DEBIT"],
+    required: true,
+  },
+
+  status: {
+    type: String,
+    enum: ["FAILED", "SUCCESS", "WAITING", "CANCELED", "PENDING"],
+    required: true,
+  },
+
+  runningBalance: {
+    wallet: {
+      type: Number,
+      required: true,
+    },
+
+    gold: {
+      type: Number,
+      required: true,
+    },
+
+    goldPrice: {
+      type: Number,
+      required: true,
+    },
+  },
+});
+
+goldTransactionSchema.statics.build = (goldTransaction: IGoldTransaction) => {
+  return new GoldTransaction(goldTransaction);
+};
+
+const GoldTransaction = mongoose.model<
+  GoldTransactionDoc,
+  GoldTransactionModel
+>("GoldTransaction", goldTransactionSchema);
+
+export { GoldTransaction };
